76c327961be8344fa2e1053a71e9ea9b181ea3ff
ivan_ <ivan_@LAPTOP-LVTM11G4>
2019-11-13 18:40:03 +0100

    Añado comentarios en la rama cadenas para explicar qué estoy haciendo

--- proyectos-netbeans/BasicosIvanZarco/src/basicosivanzarco/BasicosIvanZarco.java
+++ proyectos-netbeans/BasicosIvanZarco/src/basicosivanzarco/BasicosIvanZarco.java
@@ -10,15 +10,20 @@ package basicosivanzarco;
  * @author ivan_
  */
 public class BasicosIvanZarco {
+    /**
+     * Método para mostrar ejemplo de tipo cadena de caracteres
+     * */
 public static void cadenas() {
-
+ /*Tipo básico*/
         String palabrita;
         
-        
+     /*Asiganción de valor a la variable*/  
         palabrita = "hola";
         
         
-        
+     /*Muestra en pantalla el contenido de la variable, en concreto una palabra, que no es más que
+        una cadena de caracteres*/
+           
         System.out.print("Mi palabrita es: ");
         System.out.println(palabrita);
 
